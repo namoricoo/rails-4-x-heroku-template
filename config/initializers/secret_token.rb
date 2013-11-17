62548fafb345f44a9cf435de90982902c438ba2a
Nate <namoricoo@hotmail.com>
2013-11-17 09:03:22 -0800

    Initial commit for rails 4.x heroku template

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Rails4XHerokuTemplate::Application.config.secret_key_base = '181f56b4e6cce8218b5fcbd3c62ee1258520c2ecfc1cc931b8201cdd9ad40419f9560271b3f9c244dd4c8aca2627fa166e6b1b4548f4fdb31018897ba8faa870'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Rails4XHerokuTemplate::Application.config.session_store :cookie_store, key: '_rails-4-x-heroku-template_session'
